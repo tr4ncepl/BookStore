8f9672b2e8896081a2ec30cea68237cd70831f85
Szymon Bucki <tr4ncepl@gmail.com>
2021-07-09 12:52:57 +0200

    Dodana walidacja wprowadzanych danych\

--- BookShop.Domain/Entities/Book.cs
+++ BookShop.Domain/Entities/Book.cs
@@ -13,23 +13,33 @@ namespace BookShop.Domain.Entities
     {
         [HiddenInput(DisplayValue=false)]
         public int BookID { get; set; }
-        
+
+        [Required(ErrorMessage ="Proszę podać nazwę książki.")]
         [Display(Name ="Tytuł")]
         public string Title { get; set; }
 
+        [Required(ErrorMessage ="Proszę podać autora.")]
         [Display(Name ="Autor")]
         public string Author { get; set; }
 
+
+        [Required(ErrorMessage ="Proszę podać gatunek.")]
         [Display(Name ="Gatunek")]
         public string Genre { get; set; }
         
+
+        [Required]
+        [Range(0.01, double.MaxValue,ErrorMessage ="Proszę podać dodatnią cenę.")]
         [Display(Name ="Cena")]
         public decimal Price { get; set; }
 
         [DataType(DataType.MultilineText),Display(Name ="Opis")]
+        [Required(ErrorMessage ="Proszę podać opis.")]
         public string Description { get; set; }
 
-        [Display(Name ="Cena")]
+        [Display(Name ="Ocena")]
+        [Required]
+        [Range(0,10,ErrorMessage ="Proszę podać ocenę 0-10")]
         public decimal Rating { get; set; }
     }
 }
