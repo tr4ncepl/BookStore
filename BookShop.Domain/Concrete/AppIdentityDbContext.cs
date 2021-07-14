1b2423be63ad0a34d6107eb05693f8142b2987b7
Szymon Bucki <tr4ncepl@gmail.com>
2021-07-14 15:40:55 +0200

    Dodawanie konta admina przy tworzeniu nowego schematu bazy

--- BookShop.Domain/Concrete/AppIdentityDbContext.cs
+++ BookShop.Domain/Concrete/AppIdentityDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using BookShop.Domain.Entities;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
@@ -35,7 +36,29 @@ namespace BookShop.Domain.Concrete
         }
         public void PerformInitialSetup(AppIdentityDbContext context)
         {
-            // initial configuration will go here
+            AppUserManager userManager = new AppUserManager(new UserStore<AppUser>(context));
+            AppRoleManager roleManager = new AppRoleManager(new RoleStore<AppRole>(context));
+
+            string roleName = "admin";
+            string userName = "admin";
+            string passowrd = "Test123$";
+            string email = "admin@example.com";
+
+            if(!roleManager.RoleExists(roleName))
+            {
+                roleManager.Create(new AppRole(roleName));
+            }
+
+            AppUser user = userManager.FindByName(userName);
+            if (user == null)
+            {
+                userManager.Create(new AppUser { UserName = userName, Email = email }, passowrd);
+                user = userManager.FindByName(userName);
+            }
+            if(!userManager.IsInRole(user.Id,roleName))
+            {
+                userManager.AddToRole(user.Id, roleName);
+            }
         }
     }
 
