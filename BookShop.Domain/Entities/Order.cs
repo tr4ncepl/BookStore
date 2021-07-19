a3492b365f56607413eaa34490fdddf4a55a2ec3
Szymon Bucki <tr4ncepl@gmail.com>
2021-07-19 17:02:51 +0200

    Poprawione wyswietlanie zamowien

--- BookShop.Domain/Entities/Order.cs
+++ BookShop.Domain/Entities/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,22 @@ namespace BookShop.Domain.Entities
     {
 
         public int OrderId { get; set; }
+        [Display(Name = "Imię i nazwisko")]
         public string RecName { get; set; }
+        [Display(Name = "Ulica")]
         public string RecAdress { get; set; }
+        [Display(Name = "Miasto")]
         public string RecCity { get; set; }
+        [Display(Name = "Województwo")]
         public string RecState { get; set; }
+        [Display(Name = "Kod pocztowy")]
         public string RecZip { get; set; }
+        [Display(Name = "Kraj")]
         public string RecCountry { get; set; }
+        [Display(Name = "Ozdobne pakowanie")]
         public bool GiftWrap { get; set; }
 
+        [Display(Name = "Całkowita wartość zamówienia")]
         public decimal TotalValue { get; set; }
 
         public virtual ICollection<BookOrder> BookOrders { get; set; }
