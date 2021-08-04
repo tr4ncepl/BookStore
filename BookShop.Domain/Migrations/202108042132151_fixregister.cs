8ca16c2f10c3fb37bd9493ef5b80cda1df4d46f4
Szymon Bucki <tr4ncepl@gmail.com>
2021-08-04 23:43:14 +0200

    Naprawiony blad z identity

---
+++ BookShop.Domain/Migrations/202108042132151_fixregister.cs
@@ -0,0 +1,18 @@
+﻿namespace BookShop.Domain.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class fixregister : DbMigration
+    {
+        public override void Up()
+        {
+          
+        }
+        
+        public override void Down()
+        {
+            
+        }
+    }
+}
